61fb4f9fd0b39d9e8e7c378159c844b2f1d1c094
Carlos Anchundia <ceanchundia@sri.gob.ec>
2026-02-19 20:33:34 -0500

    primero fix

--- Release 1/Sprint 1/script/script_diseniador_v1.sql
+++ Release 1/Sprint 1/script/script_diseniador_v1.sql
@@ -4,7 +4,9 @@
    Motor: Oracle Database
    
    CASO DE USO: Formulario 104 - Declaración IVA Mensual SRI Ecuador
-   ================================================================ */
+   ================================================================ 
+   revisa a profundidad este diagrama de base de datos que debe servir para crear una aplicacion que realiza un diseñador que realiza diseño dinamico de formularios con oracla y vue, si tienes algo que modificar en el sql dime, creame un diagrama y una guia del paso a paso para contruir este sistema, si puedes hacer un html mejor; /*
+   */
 
 
 /* ================================================================
